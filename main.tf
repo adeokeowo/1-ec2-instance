c5672620cdc84e6fdc1768e958d916703c240e60
Ade Okeowo <ade.okeowo@foundation30.com>
2018-08-14 21:40:28 +0100

    first commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-test-instance" {
-  ami             = "${data.aws_ami.amazonlinux.id}"
+  ami             = "ami-b8b45ddf"
   instance_type   = "t2.micro"
 
   tags {
